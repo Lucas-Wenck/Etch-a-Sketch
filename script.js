3f0a4fe2b19a18c4fa57f2cf0d5bce3774be306a
Lucas Wenck <lucas.wenck@gmail.com>
2023-02-28 19:02:58 -0300

    Scrap previous idea and recreate proportion choosing

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
 let columns = 16;
 let rows = 16;
-let row;
-const grid = document.createElement('div');
-grid.setAttribute('id', 'grid');
-for(let i = 0; i < columns; i++){
-    let column = document.createElement('div');
-    column.className = 'column';
-    for(let j = 0; j < rows; j++){
-        row = document.createElement('div');
-        row.className = 'row';
-        row.innerHTML = '';
-        column.appendChild(row);
+let grid;
+const body = document.getElementById('body');
+function baseGrid(){
+    grid = document.createElement('div');
+    grid.setAttribute('id', 'grid');
+    for(let i = 0; i < columns; i++){
+        let column = document.createElement('div');
+        column.className = 'column';
+        for(let j = 0; j < rows; j++){
+            let row = document.createElement('div');
+            row.className = 'row';
+            row.innerHTML = '';
+            column.appendChild(row);
+        }
+        grid.appendChild(column);
     }
-    grid.appendChild(column);
-}
-document.body.appendChild(grid);
-
-const hover = document.querySelectorAll('.row');
-hover.forEach(row => {
+    document.body.appendChild(grid);
+    const hover = document.querySelectorAll('.row');
+    hover.forEach(row => {
     row.addEventListener('mouseover', function backgroundChange(event){
         row.style.backgroundColor = 'black';
+        })
     })
+}
+baseGrid()
+function click(){
+    body.removeChild(grid);
+    grid = document.createElement('div');
+    grid.setAttribute('id', 'grid');
+    let columns = Number(prompt(`How many columns should the grid have?`, `16`));
+    let rows = Number(prompt(`how many rows should the grid have?`, `16`));
+        for(let i = 0; i < columns; i++){
+        let column = document.createElement('div');
+        column.className = 'column';
+        for(let j = 0; j < rows; j++){
+            let row = document.createElement('div');
+            row.className = 'row';
+            row.innerHTML = '';
+            column.appendChild(row);
+        }
+        grid.appendChild(column);
+    }
+    document.body.appendChild(grid);
+    const hover = document.querySelectorAll('.row');
+    hover.forEach(row => {
+    row.addEventListener('mouseover', function backgroundChange(event){
+        row.style.backgroundColor = 'black';
+        })
+    })
+}
+
+const proportions = document.getElementById('proportions')
+proportions.addEventListener('click', () => {
+    click();
 })
-
-const row_number = document.getElementById('row-number');
-for(let m = 16; m <= 100; m++){
-    let rowOptions = document.createElement('option');
-    rowOptions.value = `${m}`;
-    rowOptions.text = `${m}`;
-    row_number.add(rowOptions);
-}
-
-const column_number = document.getElementById('column-number');
-for(let n = 16; n <= 100; n++){
-    let colOptions = document.createElement('option');
-    colOptions.value = `${n}`;
-    colOptions.text = `${n}`;
-    column_number.add(colOptions);
-}
